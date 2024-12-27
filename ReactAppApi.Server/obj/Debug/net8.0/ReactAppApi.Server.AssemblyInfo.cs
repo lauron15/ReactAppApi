bdf75cad77289c30b24773d1fb7d62f3be41cef2
Lauron15 <lauroafp@gmail.com>
2024-12-27 18:33:55 -0300

    Just translated all the comment to English.

--- ReactAppApi.Server/obj/Debug/net8.0/ReactAppApi.Server.AssemblyInfo.cs
+++ ReactAppApi.Server/obj/Debug/net8.0/ReactAppApi.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactAppApi.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c44245293687b13c5e652cc9ddd768c23551467")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881f2f59f768fbe47aaccf476bd673f01420eb0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactAppApi.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactAppApi.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
