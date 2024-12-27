3f495497d918da1f42629ae7bf2bbde08ff50dce
Lauron15 <lauroafp@gmail.com>
2024-12-27 18:11:49 -0300

    stockFix

---
+++ ReactAppApi.Server/Models/Stock.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReactAppApi.Server.Models
+{
+    public class Stock
+    {
+
+        //DO not use data validation direct on the Models 
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("Symbol")]
+        public string Symbol { get; set; } = string.Empty;
+
+        [Column("company_name")] // Ajustado para evitar espaços no nome da coluna
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")] // Corrigido o tipo de dado
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")] // Corrigido o tipo de dado
+        public decimal LastDiv { get; set; }
+
+        [Column("industry")]
+        public string Industry { get; set; } = string.Empty;
+
+        [Column("market_cap")] // Ajustado para evitar espaços no nome da coluna
+        public long MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+           
+    }
+}
